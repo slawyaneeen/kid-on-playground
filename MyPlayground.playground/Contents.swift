ceeb1ceb19492a612808d2238e20f688b0316482
slawyaneeen <42421436+slawyaneeen@users.noreply.github.com>
2022-12-06 18:19:18 +0500

    upd 2
    rat 2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 import CoreGraphics
+import Darwin
 
 var greeting = "Hello, playground"
 1231copy(self: <#T##CGFont#>, withVariations: <#T##CFDictionary?#>)
 ratttt
+new pathconf(<#T##Int8#>, <#T##Int32#>)
